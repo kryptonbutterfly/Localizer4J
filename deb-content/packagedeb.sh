fb8afc4893198a0b594c45a8cc981a71c85d3644
tinycodecrank <tinycodecrank@gmail.com>
2023-12-17 16:32:12 +0100

    create maven nature
    update .deb build script

--- deb-content/packagedeb.sh
+++ deb-content/packagedeb.sh
@@ -12,9 +12,10 @@ echo ${pwd}
 VERSION_FILE="version.txt"
 
 APP_NAME="Localizer4J"
+ARTIFACT_NAME="localizer4j"
 PACKAGE_NAME=$APP_NAME
 PACKAGE_VERSION=$(<$VERSION_FILE) 
-SOURCE_DIR=$PWD/../
+SOURCE_DIR=$PWD/..
 DEPLOY_DIR="/home/$SUDO_USER/ownCloud/Applications"
 TEMP_DIR="/tmp"
 
@@ -33,7 +34,7 @@ cat control >> $TEMP_DIR/debian/DEBIAN/control
 cp $APP_NAME.desktop $TEMP_DIR/debian/usr/share/applications/
 cp langProject.xml $TEMP_DIR/debian/usr/share/mime/packages/
 
-cp $SOURCE_DIR/build/$APP_NAME.jar $TEMP_DIR/debian/usr/share/$PACKAGE_NAME/
+cp $SOURCE_DIR/target/$ARTIFACT_NAME-$PACKAGE_VERSION.jar $TEMP_DIR/debian/usr/share/$PACKAGE_NAME/$APP_NAME.jar
 
 echo "$PACKAGE_NAME ($PACKAGE_VERSION) trusty; urgency=low" > changelog
 echo "  * Rebuild" >> changelog
@@ -55,8 +56,10 @@ chgrp -R root $TEMP_DIR/debian/
 
 cd $TEMP_DIR/
 dpkg --build debian
+
+mkdir -p $SOURCE_DIR/build
 mv $TEMP_DIR/debian.deb $SOURCE_DIR/build/$PACKAGE_NAME-$PACKAGE_VERSION.deb
 rm -r $TEMP_DIR/debian
 
 cp $SOURCE_DIR/build/$PACKAGE_NAME-$PACKAGE_VERSION.deb $DEPLOY_DIR/Deb/
-cp $SOURCE_DIR/build/$APP_NAME.jar $DEPLOY_DIR/
+cp $SOURCE_DIR/target/$ARTIFACT_NAME-$PACKAGE_VERSION.jar $DEPLOY_DIR/
